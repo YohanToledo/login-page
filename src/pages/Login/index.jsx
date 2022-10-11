1c494e1b88b814627deb79655b238b0e57c37eda
Yohan Gabriel Toledo Bernart <yohan.gabriel.bernart@gmail.com>
2022-10-11 14:02:35 +0000

    feat: graph icons

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,9 @@
 import Form from "../../components/LoginForm";
 import "./login.css";
 import { BsGraphUp } from "react-icons/bs";
+import { GoGraph } from "react-icons/go";
+import { VscGraph } from "react-icons/vsc";
+import { MdAutoGraph } from "react-icons/md";
 
 const Login = () => {
   return (
@@ -9,6 +12,15 @@ const Login = () => {
         <div className="left-column">
           <div className="icon">
             <BsGraphUp />
+          </div>
+          <div className="icon">
+            <GoGraph />
+          </div>
+          <div className="icon">
+            <VscGraph />
+          </div>
+          <div className="icon">
+            <MdAutoGraph />
           </div>
         </div>
         <div className="right-column">
