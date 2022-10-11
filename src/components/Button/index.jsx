7244bc515ef441f662d77edc7a2c1373255e1e1d
Yohan Gabriel Toledo Bernart <yohan.gabriel.bernart@gmail.com>
2022-10-11 13:24:09 +0000

    feat: login input icons

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,7 +4,7 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 const Button = ({ isLoading, children }) => {
   return (
     <>
-      <button type="submit" className="login-button">
+      <button type="submit" className="login-button" disabled={isLoading}>
         {isLoading ? (
           <AiOutlineLoading3Quarters className="load-icon" />
         ) : (
--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Button from "../Button";
 import "./loginform.css";
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 
 const Form = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -11,23 +13,28 @@ const Form = () => {
 
   return (
     <>
-      <div className="login-form" onSubmit={handleSubmit}>
-        <form className="form">
+      <div className="login-form">
+        <form className="form" onSubmit={handleSubmit}>
           <div className="row">
             <label className="w-90">Usuário</label>
           </div>
           <div className="row">
+            <i className="input-icon">
+              <FaUser />
+            </i>
             <input type="text" className="input-text font" />
           </div>
           <div className="row">
             <label className="w-90">Senha</label>
           </div>
           <div className="row">
+            <i className="input-icon">
+              <FaLock />
+            </i>
             <input type="password" className="input-text font" />
           </div>
           <div className="row">
             <Button isLoading={isLoading}> Login </Button>
-            
           </div>
         </form>
       </div>
